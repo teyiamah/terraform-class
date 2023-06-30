a5f22e9f98d2c60828a8bfe8c07e732127051b79
amah <kangni1993@gmail.com>
2023-06-30 14:03:02 -0400

    terraform

---
+++ terraform.tfvars
@@ -0,0 +1,6 @@
+my_region     = "us-east-2"
+profile       = "default"
+topic_name    = "sns-dev"
+endpoint      = "sghshfnrunrbf@gmail.com" #please replace email
+protocol_type = "email"
+team          = "dev"
---
+++ variable.tf
@@ -0,0 +1,29 @@
+variable "my_region" {
+  type    = string
+  default = "us-east-1"
+
+}
+
+variable "profile" {
+  default = "default"
+
+}
+
+variable "topic_name" {
+  default = "sns-dev"
+
+}
+
+variable "team" {
+  default = "dev"
+
+}
+
+variable "protocol_type" {
+  default = "email"
+
+}
+variable "endpoint" {
+  default = "kangni1993@gmail.com"
+
+}
